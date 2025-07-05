0d5b75ee03d83a448b41a9bb4ef380ffd8372544
acer <acer@emadmukhtar.com>
2025-07-05 05:15:16 +0400

    fix(project): cd into wrong folder to run terraform should be app-terraform but i was login into infra-terraform add(backend): set terraform backend

---
+++ app-terraform/backend.tf
@@ -0,0 +1,15 @@
+terraform {
+  # Production development
+  # backend "s3" {
+  #   key = "web-server"
+  #   bucket = "terraform-backend-terraformbackends3bucket-9r8ui0vz9fhl"
+  #   region = "us-east-1"
+  #   dynamodb_table = "terraform-backend-TerraformBackendDynamoDBTable-1R1MC4XTVNJGP"
+  # }
+
+  # Local development
+  backend "local" {
+    path = "terraform.tfstate"
+    region = "ap-northeast-1"
+  }
+}
